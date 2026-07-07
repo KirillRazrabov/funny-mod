03eef5e1a5ac1a39f020693544cedec058714909
KirillRazrabov <zerpetyt@gmail.com>
2026-07-07 09:25:04 +0500

    - version 1.0.2 - Creative Tab Menu - adding eu_us lang and name for item - Some fixes

--- src/main/java/name/modid/FunnyMod.java
+++ src/main/java/name/modid/FunnyMod.java
@@ -1,15 +1,14 @@
 package name.modid;
 
 import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
-
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,25 +16,38 @@ import org.slf4j.LoggerFactory;
 import java.util.function.Function;
 
 public class FunnyMod implements ModInitializer {
+
 	public static final String MOD_ID = "funny-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Initializing Funny Mod...");
 
-		LOGGER.info("Hello Fabric world!");
 		ModItems.initialize();
+
+		Registry.register(
+				BuiltInRegistries.CREATIVE_MODE_TAB,
+				CUSTOM_CREATIVE_TAB_KEY,
+				CUSTOM_CREATIVE_TAB
+		);
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
 	}
 
 	public static class ModItems {
 
 		public static final Item SUSPICIOUS_SUBSTANCE =
-				register("suspicious_substance", Item::new, new Item.Properties());
+				register(
+						"suspicious_substance",
+						Item::new,
+						new Item.Properties()
+				);
 
 		public static void initialize() {
-			FunnyMod.LOGGER.info("Registering items...");
-			CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS)
-					.register((creativeTab) -> creativeTab.accept(ModItems.SUSPICIOUS_SUBSTANCE));
+			LOGGER.info("Registering items...");
 		}
 
 		private static <T extends Item> T register(
@@ -50,12 +62,26 @@ public class FunnyMod implements ModInitializer {
 
 			T item = factory.apply(settings.setId(key));
 
-			Registry.register(BuiltInRegistries.ITEM, key, item);
+			Registry.register(
+					BuiltInRegistries.ITEM,
+					key,
+					item
+			);
 
 			return item;
 		}
 	}
-	public static Identifier id(String path) {
-		return Identifier.fromNamespaceAndPath(MOD_ID, path);
-	}
+
+	public static final ResourceKey<CreativeModeTab> CUSTOM_CREATIVE_TAB_KEY =
+			ResourceKey.create(
+					Registries.CREATIVE_MODE_TAB,
+					id("creative_tab")
+			);
+
+	public static final CreativeModeTab CUSTOM_CREATIVE_TAB =
+			FabricCreativeModeTab.builder()
+					.title(Component.translatable("itemGroup.funny-mod"))
+					.icon(ModItems.SUSPICIOUS_SUBSTANCE::getDefaultInstance)
+					.displayItems((_, output) -> output.accept(ModItems.SUSPICIOUS_SUBSTANCE))
+					.build();
 }
